06c80a389d226c617d3df0e9af4ad685c2725ead
Atharv Vatsal <atharvvatsal@outlook.com>
2024-03-27 09:32:33 +0530

    Test Code File for C++

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -6,13 +6,7 @@ int main() {
     int count = 5;
     for (int i = 0; i < count; i++)
     {
-        if (i == 3)
-        {
-            continue;
-        }
-        cout<<i<<endl;
-        
-        
+ 
     }
     
     return 0;
